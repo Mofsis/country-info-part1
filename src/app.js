cf8e42e1f66a3e61dc5c8e68c6ecd7409dc9342e
mofsis <mtanios@hotmail.com>
2021-11-08 16:23:17 +0100

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log('Hallo daar!');
+import axios from 'axios';
+
+async function getCountries() {
+    try {
+        const result = await axios.get("https://restcountries.com/v2/all")
+        console.log(result);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+getCountries();
+
+
+const listOfCountries = document.getElementById("list")
+fetch("https://restcountries.com/v2/all")
+    .then(res => res.json())
+    .then(data => {
+        const countries = data.sort((a,b) => a.population - b.population)
+            .map((country) => {
+            return `<li class="region-${country.region}"><img class="flag" src="${country.flag}" alt="flag">${country.name}
+                <p class="pop">Has a population of ${country.population} people</p>
+            </li> `;
+        });
+
+        listOfCountries.innerHTML = `${countries.join('')}`;
+    });
+
+
+
